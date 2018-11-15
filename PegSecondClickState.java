33c23b53144e0102a46ef9cb558c08abfe57d09f
Paul Jones <chickenjones44@gmail.com>
2018-11-15 14:30:59 -0600

    first

---
+++ PegSecondClickState.java
@@ -0,0 +1,55 @@
+
+public class PegSecondClickState implements PegState
+{
+   private Pegs pegs;
+   private int start;  //remember which peg was clicked on (info forwarded from PegFirstClickState)
+
+   public PegSecondClickState(Pegs pegs)
+   {
+
+   
+   
+   
+      start = -1;
+   }
+
+   //method called by PegFirstClickState
+   public void setStart(int start)
+   {
+      this.start = start;
+   }
+
+   public void mouseClicked(int x, int y)
+   {
+      //find which slot was clicked on for the second mouse click
+      int end = 
+
+      Jump jump = pegs.getJump(start, end);
+
+      //perform the jump (refer to PegFirstClickState)
+      //check for end of game
+      if (jump != null)  
+      {
+
+		jump.jump(pegs);
+         pegs.incrementTurn();
+         if (pegs.gameOver())
+         {
+            
+			
+			
+			
+			
+            return;
+         }
+      }
+
+      //reset to the first click state (reset start as well)
+
+
+	  
+	  
+	start = -1;
+
+   }
+}
